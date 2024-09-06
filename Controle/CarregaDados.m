9b530aed5125bdac715fff6de84d1464ccf4aa12
Galdir Reges <galdir@gmail.com>
2024-09-06 08:14:23 -0300

    testes com tabela pchegada até 51

--- Controle/CarregaDados.m
+++ Controle/CarregaDados.m
@@ -14,18 +14,14 @@ close all
 % Carrega as tabelas de referências da Petrobras para as proteções e para
 % buscar condiçoes iniciais em pontos de operação reais
 CarregaTabelas; 
-TabRestricoesDinamicas=readtable('DP.xlsx');              % Tabela de valores de referência para as variáveis
-FxPercent=readtable('DP-Faixas.xlsx');                           % Tabela das faixas percentuais para disparar os alarmes LL, L, H e HH
-BTP=readtable('DoBTP.xlsx');                                           % Tabela de dados do teste de produção
-ProtecaoFixa =readtable('FixedProtections.xlsx');            % Carrega tabelas de proteção fixa da Petrobras
-TabSimulador=LeConverteNomes('DoSimulador.xlsx');  % Tabela do simulador (Análise de Sensibilidade)
 TabSimulador_ate51=LeConverteNomes('DoSimulador - ate_pchegada_51.xlsx');  % Tabela do simulador (Análise de Sensibilidade)
-
+TabSimulador = TabSimulador_ate51;
 %% =============================================================================
 % Condição inicial  das variáveis do processo e das entradas     PSuc [bar],    PChegada [bar]        Freq [Hz]
 %  [YIni,UIni]=SelCondicaoInicial('2024-07-12 10:00:00');          % PSuc=77.4    PChegada=31.4      Freq = 54.9Hz
 % [YIni,UIni]=SelCondicaoInicial('2024-07-12 15:45:00');          % PSuc=78.9    PChegada=34.2      Freq = 53.9Hz
  [YIni,UIni]=SelCondicaoInicial('2024-07-17 00:00:00');               % PSuc=97.4    PChegada=35.3      Freq = 40Hz      Rampa de aceleração
+%[YIni,UIni]=SelCondicaoInicial('2024-07-17 01:00:00');               %Rampa de aceleração
 
 % A condição inicial da vazão precisamos apenas para inicializar a visualização dos mapas  
 VazaoIni=Interpola(UIni(1),YIni(2)*1.019716,TabSimulador,3);  % Entra Freq [Hz] e Presão [Kgf/cm2] para retornar a vazão em m3/dia
@@ -52,8 +48,8 @@ PMonAlvoIni=Plano.PMonAlvo(1);                    % Resgata da tabela o ponto de
 % Carrega uma rede (ESN qualquer) para ser usada como modelo do processo
 % Rede_Processo = load('.\ESN\weightsESNx_JUB27n100.mat');
 Rede_Processo = load('..\Modelos\ESN\weightsESNx_TR300_TVaz0.8_RaioE0.1.mat');
-% Rede_Processo = load('.\ESN\weightsESNx_TR400_TVaz0.9_RaioE0.4.mat');
-% Rede_Processo = load('.\ESN\weightsESNx_TR900_TVaz0.9_RaioE0.4.mat');
+%Rede_Processo = load('..\Modelos\\ESN\weightsESNx_TR400_TVaz0.9_RaioE0.4.mat');
+%Rede_Processo = load('.\ESN\weightsESNx_TR900_TVaz0.9_RaioE0.4.mat');
 
 
 % Vale a pena inserir ruido na saida do processo para simular mundo real e avaliar robustez do controlador
@@ -65,7 +61,7 @@ TipoRede=1;        % 1=ESN ou  2=LSTM
 
 % Carrega rede ESN que será utilizada como preditor no MPC
 % ESN_MPC = load('.\ESN\weightsESNx_JUB27n100.mat');
-% ESN_MPC = load('.\ESN\weightsESNx_TR300_TVaz0.8_RaioE0.1.mat');
+% ESN_MPC = load('..\Modelos\ESN\weightsESNx_TR300_TVaz0.8_RaioE0.1.mat');
 ESN_MPC = load('..\Modelos\ESN\weightsESNx_TR400_TVaz0.9_RaioE0.4.mat');
 ESN_MPC.data.tipo = TipoRede;
 ModeloPreditor = ESN_MPC;
@@ -93,14 +89,14 @@ PMonAlvoMaxMin=[40; 25];
 umax  = [FreqMaxMin(1); PMonAlvoMaxMin(1)];       % Valor máximo das manipuladas (Freq e PMonAlvo)
 umin  =  [FreqMaxMin(2); PMonAlvoMaxMin(2)] ;      % Valor mínimo das manipuladas  (Freq e PMonAlvo)
  
-dumax = [0.9 ; 1];                                                          %Variação máxima nas manipuladas [ Hz    bar ]
+dumax = [0.5 ; 1];                                                          %Variação máxima nas manipuladas [ Hz    bar ]
 dumin = [0; 0];                                                               %Variação mínima nas manipuladas [ Hz    bar ]
 
 %% ======================
 % Parâmetros do Controlador
 Hp =  10;                     % Horizonte de predição
 Hc =  4 ;                      % Horizonte de controle
-Qy=  [1 1];                   % Qy - Peso das Controladas (PSuc e PChegada) prioridade da variável controlada voltar para respectiva faixa (calculada pelas proteções dinâmicas)
+Qy=  [2 1];                   % Qy - Peso das Controladas (PSuc e PChegada) prioridade da variável controlada voltar para respectiva faixa (calculada pelas proteções dinâmicas)
 R=    [1 1];                   % R - Peso na variação da ação de controle - Delta U em (Freq. e PMonAlvo) 
 Qu = [1 1];                   % Qu - Peso dos Alvos Desejados (Freq. e PMonAlvo). Ponderação priorização dos alvos (ponto do mapa)
 
@@ -115,3 +111,5 @@ InicializaMPC=[MatrizYIni(:);DeltaUIni;YIni(1:2)];   % Monta vetor de estados in
 %% =============================================================================
 disp('Configurações do usuário e parâmetros do controlador foram carregados para a área de trabalho')
 
+disp("Python acessível no Matlab?:");
+disp(pyenv);
