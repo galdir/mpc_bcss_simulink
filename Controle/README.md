b863e914d13d45fa5b9ba4459113fc26fb6f8bc1
LeizerSch <LEIZER@UFBA.BR>
2024-09-13 16:45:05 -0300

    Vá atrás !!!

--- Controle/README.md
+++
@@ -1,2 +0,0 @@
-# mpc_bcss_simulink
- 
--- Uteis/predict_LSTM_Casadi.m
+++
@@ -1,6 +0,0 @@
-function res = predict_LSTM_Casadi(modelo_casadi, x_input)
-
-    res = modelo_casadi.casadi_func(x_input, modelo_casadi.h0_input, modelo_casadi.c0_input);
-    res=res(end);
-
-end
