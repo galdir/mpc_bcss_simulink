d091446d5ec7a8bffe262dffa93ffae0df84eb67
caiogmello <caiogomedemello@gmail.com>
2024-11-22 16:02:51 -0300

    alteraçao diretorio dos dados

--- AjustaPath.m
+++ AjustaPath.m
@@ -2,8 +2,8 @@
 addpath('.\Tabelas');             % Tabelas XLS Petrobras
 addpath('.\Uteis');                  % Funcionalidade de utilidade geral consumida por vários
 addpath('.\Modelos\ESN');    % Funcionalidade de utilidade geral consumida por vários
-% addpath('.\Casadi');           % Funções da biblioteca Casadi
-addpath('..\Dados');               % Arquivos parquet
+addpath('.\Casadi');           % Funções da biblioteca Casadi
+addpath('C:\petrobras_2023_sistema_controle_inteligente_operacao_BCS_campo\DADOS UTEIS\dados MPA OPC');               % Arquivos parquet
 
 % Já desloca para a pasta em uso corrente
 cd .\Controle
